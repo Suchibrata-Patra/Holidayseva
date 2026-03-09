263d4d0b76b11c4178fd75ddba5c8c4c42494a5b
Suchibrata Patra <101924210+Suchibrata-Patra@users.noreply.github.com>
2026-03-09 13:10:03 +0530

    cc

--- hr.holidayseva.com/default.php
+++ hr.holidayseva.com/default.php
@@ -2,10 +2,8 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<title>SXC MDTS</title>
+<title>Page moved</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="icon" href="https://holidayseva.com/Assets/favicon.ico" type="image/x-icon">
-
 
 <style>
 
